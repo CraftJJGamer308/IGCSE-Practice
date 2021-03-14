ed56069c49b8f3185fb9d0aa7279da7a6e904e57
BuildTools <mr4117@outlook.kr>
2021-03-14 15:55:19 +0100

    Update program.py

--- 2/program.py
+++ 2/program.py
@@ -8,17 +8,17 @@ for i in range(0, TGcddNum):
     TGcdd.append(input('enter the name of candidate '+str(i+1)+': '))
 
 for i in range(0, TGstdNum):
-    for j in range(0, len(TGcdd)):
+    for j in range(0, TGcddNum):
         print(str(j+1)+'. '+str(TGcdd[j]))
     TGvotes[int(input('enter your choice: '))-1] += 1
 
 print('\n\nThe Voting Results for '+TGname)
-for i in range(0, len(TGcdd)):
+for i in range(0, TGcddNum):
     print(str(i+1)+'. '+str(TGcdd[i])+': '+str(TGvotes[i])+'votes')
 
-mx=max(TGvotes)
-winners=[]
-for i in range(0,len(TGcdd)):
-    if TGvotes[i]==mx:
+mx = max(TGvotes)
+winners = []
+for i in range(0, TGcddNum):
+    if TGvotes[i] == mx:
         winners.append(TGcdd[i])
 print('\nThe winner(s) is/are '+str(', '.join(winners)))
