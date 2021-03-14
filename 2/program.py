a52e3e37c630eaa3d60149b92a1c4c388061e502
Kn1pz <79281601+Kn1pz@users.noreply.github.com>
2021-03-14 17:41:41 +0100

    Update program.py

--- 2/program.py
+++ 2/program.py
@@ -1,73 +1,87 @@
-TGname = input('enter the name of the tutor group: ')
-TGstdNum = 0
+TGname = input('enter the name of the tutor group: ') #Tutor group name
+TGstdNum = 0 #Tutor Group Amount of students
 
-def getTGstdNum():
+def getTGstdNum(): 
+    #function for amount of students
     global TGstdNum
     TGstdNum =int(input('enter the number of students in the tutor group:'))
     return TGstdNum
 
 # while not(28<=getTGstdNum()<=35): 
-while not(getTGstdNum()<=35): #for now
+while not(getTGstdNum()<=35): #for now      
+    #checks if amount of students is between 28 and 35
     print('Please try again')
 
-TGcddNum=0
+TGcddNum=0 #Tutor group amount of candidates
 
-def getTGcddNum():
+def getTGcddNum():      
+    #function for amount of candidates
     global TGcddNum
     TGcddNum = int(input('enter the number of candidates (max 4):'))
     return TGcddNum
 
-while not(1<=getTGcddNum()<=4):
+while not(1<=getTGcddNum()<=4):        
+    #checks if the amount of candidates is in the required slot
     print('please try again')
+     
 print(TGcddNum)
-TGcdd = []
-TGvotes = [0, 0, 0, 0]
+
+
+TGcdd = []  #array of candidates
+TGvotes = [0, 0, 0, 0] #counts of vote per candidate
 
 for i in range(0, TGcddNum):
+    #enter candidate names
     TGcdd.append(input('enter the name of candidate '+str(i+1)+': '))
 
 
-vtrNo = []
-abstained = 0
+vtrNo = []  #voter Number (like student number)
+abstained = 0 
 
-def voting():
+def voting(): 
+    #getting input of votes per voter number
     global abstained
     global TGstdNum
     global TGcddNum
     global TGcdd
-    print('0. obstain')
-    for j in range(0, TGcddNum):
-        print(str(j+1)+'. '+str(TGcdd[j]))
+    
+    
+    print('0. abstain') #prints 0. abstain
+    for j in range(0, TGcddNum): #index starts at 0
+        print(str(j+1)+'. '+str(TGcdd[j])) #index + 1, candidate name presented looks like 1. asdfjaslf
     vote = int(input('enter your choice: '))
     
-    if vote ==0:
+    if vote ==0: 
+        #checks for abstained votes
         abstained+=1
-        return
-    TGvotes[vote-1] += 1
+    else:
+        TGvotes[vote-1] += 1 #inputs the vote into the vote array
 
-def getStdNo():
+def getVtrNo(): #function for getting votes while checking voter number
     global vtrNo
-    response = int(input('enter your voter number: '))
+    response = int(input('enter your voter number: ')) #checks vtrNO
     while (response in vtrNo):
+        #if vtrNo is already given once, it won't accept
         print('you have already voted')
         response = int(input('enter your voter number: '))
-    vtrNo.append(response)
-    voting()
+    vtrNo.append(response) #adds voter number to the array of voter numbers
+    voting() 
     return
 
 for i in range(0,TGstdNum):
-    getStdNo()
+    #get votes while checking voter number
+    getVtrNo()
 
-print('\n\nThe Voting Results for '+TGname)
+print('\n\nThe Voting Results for '+TGname) #Results for that tutor group
 
-for i in range(0, TGcddNum):
-    print(str(i+1)+'. '+str(TGcdd[i])+': '+str(TGvotes[i])+' votes')
+for i in range(0, TGcddNum): #show results for each candidate from the amount of candidates
+    print(str(i+1)+'. '+str(TGcdd[i])+': '+str(TGvotes[i])+' votes') #index at 0, voter was given at 1 ad onwards; gets candidate name; gets candidate votes
 
-mx = max(TGvotes)
-winners = []
-for i in range(0, TGcddNum):
-    if TGvotes[i] == mx:
+mx = max(TGvotes) #searches for winner by the maximum value in the votes
+winners = [] #array for the winners
+for i in range(0, TGcddNum): #getting index of the winners
+    if TGvotes[i] == mx: #if candidate belongs to the winners, add it 
         winners.append(TGcdd[i])
 
-print('\nabstained votes: '+str(abstained))
-print('The winner(s) is/are '+str(', '.join(winners)))
+print('\nabstained votes: '+str(abstained)) #print amount of students abstained
+print('The winner(s) is/are '+str(', '.join(winners))) #print winners
