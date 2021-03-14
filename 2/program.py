bc603c91074d24ba7aaca40d9a437346d8107acc
BuildTools <mr4117@outlook.kr>
2021-03-14 17:53:17 +0100

    finally finished task 2

--- 2/program.py
+++ 2/program.py
@@ -48,8 +48,8 @@ def getVtrNo(): #function for getting votes while checking voter number
     return
 
 
-# while not(28<=getTGstdNum()<=35): 
-while not(getTGstdNum()<=35): #for now      
+while not(28<=getTGstdNum()<=35): 
+#while not(getTGstdNum()<=35): #for now      
     #checks if amount of students is between 28 and 35
     print('Please try again')
 
